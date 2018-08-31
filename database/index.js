125d74b6a4589949c0e649db83f23afe20904510
BarkerAW83 <BarkerAW@gmail.com>
2018-08-31 16:34:44 -0400

    "deployment modification 4:34p"

--- database/index.js
+++ database/index.js
@@ -5,7 +5,7 @@ const dbToken = require('../config.js').dbToken;
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/refinedToDoList', { useNewUrlParser: true }); //database for local use
 
-mongoose.connect(`mongodb://${user}:${dbToken}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true }); //database for remote use
+mongoose.connect(`mongodb://${process.env.dbUsername || user}:${process.env.dbPassword || dbToken}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true }); //database for remote use
 
 
 var db = mongoose.connection;
