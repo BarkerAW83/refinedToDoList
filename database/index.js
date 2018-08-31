5dfc059d3a84175f2607e89133f33127495eb63d
BarkerAW83 <BarkerAW@gmail.com>
2018-08-31 11:19:45 -0400

    "further deployment modification"

--- database/index.js
+++ database/index.js
@@ -3,16 +3,16 @@ const dbToken = require('../config.js').dbToken;
 
 
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/refinedToDoList', { useNewUrlParser: true }); //database for local use
+//mongoose.connect('mongodb://localhost:27017/refinedToDoList', { useNewUrlParser: true }); //database for local use
 
-mongoose.connect(`mongodb://${user}:${dbToken}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://${user}:${dbToken}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true }); //database for remote use
 
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
-  console.log('toDoList DB connected!')
+  console.log('toDoList DB connected!', ', user is: ', user, ', dbToken is: ', dbToken)
 });
 
 var refinedToDoSchema = new mongoose.Schema({
