5deed73dc76b66b2924127f9bd2dd4dbc9fd42d1
BarkerAW83 <BarkerAW@gmail.com>
2018-08-31 16:50:50 -0400

    "deployment modification 4:51p"

--- database/index.js
+++ database/index.js
@@ -5,7 +5,10 @@
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/refinedToDoList', { useNewUrlParser: true }); //database for local use
 
-mongoose.connect(`mongodb://${process.env.dbUsername || user}:${process.env.dbPassword || dbToken}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true }); //database for remote use
+//mongoose.connect(`mongodb://${user}:${dbToken}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true }); //database for local use
+
+mongoose.connect(`mongodb://${process.env.dbUsername}:${process.env.dbPassword}@ds137862.mlab.com:37862/barkeraw83refinedtodo`, { useNewUrlParser: true }); //database for remote use
+
 
 
 var db = mongoose.connection;
--- server/index.js
+++ server/index.js
@@ -33,4 +33,4 @@ app.delete('/todos', function(req, res){
 
 var port = process.env.port || 3000;
 
-app.listen(port, () => console.log('refined To-Do List server listening on port 3000')) // for local app use
+app.listen(port, () => console.log(`refined To-Do List server listening on port ${port}`)) // for local app use
